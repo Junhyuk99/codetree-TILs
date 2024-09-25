d1d9378d87493962459500e6c2fb0d4b32b6303e
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-09-25 14:46:23 +0900

    Title: 출석 부르기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/출석 부르기/calling-attendance.py
+++ 240925/출석 부르기/calling-attendance.py
@@ -1,7 +1,4 @@
 n = int(input())
-if n==1: 
-    print("John")
-elif n==2: 
-    print("Tom")
-elif n==3: 
-    print("Tom")
+if n==1: print("John")
+elif n==2: print("Tom")
+elif n==3: print("Paul")
