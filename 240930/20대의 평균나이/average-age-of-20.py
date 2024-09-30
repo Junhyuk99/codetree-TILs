aa7072523ffdd797089c37f7f48e267f936894fb
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-09-30 10:10:24 +0900

    Title: 20대의 평균나이, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240930/20대의 평균나이/average-age-of-20.py
+++ 240930/20대의 평균나이/average-age-of-20.py
@@ -3,7 +3,8 @@ cnt = 0
 while True:
     n = int(input())
     if n >=30 or n <= 20:
-        print (f"{total/cnt:.2f}")
+        cal = total/cnt
+        print ("%.2f" %cal)
         break
     cnt += 1
     total += n
