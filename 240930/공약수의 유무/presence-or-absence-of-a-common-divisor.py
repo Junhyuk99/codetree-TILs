f64b14082d6a36efe9551d1d94f3f7bb8ebf3656
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-09-30 11:04:34 +0900

    Title: 공약수의 유무, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240930/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240930/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,17 +1,9 @@
 a,b = map(int, input().split())
-arr=[]
 cnt = 0
-for i in range(1, int(1920**(2/1))+1):
-    if 1920%i==0:
-        if 2880%i==0:
-            arr.append(1920//i)
-            arr.append(i)
-arr.sort()
-for i in range(a, b+1):
-    for j in range(len(arr)):
-        if i==arr[j]:
-            print(1)
-            cnt += 1
-            break
+for i in range (a,b+1):
+    if (1920%i==0 and 2880%i==0):
+        print(1)
+        cnt += 1
+        break
 if cnt==0:
     print(0)
