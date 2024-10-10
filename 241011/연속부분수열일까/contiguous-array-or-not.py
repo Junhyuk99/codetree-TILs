8d7a831616fcfa66863d50c18b6606b4a024f234
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-10-11 08:59:05 +0900

    Title: 연속부분수열일까, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/연속부분수열일까/contiguous-array-or-not.py
+++ 241011/연속부분수열일까/contiguous-array-or-not.py
@@ -1,6 +1,8 @@
 n1, n2 = map(int, input().split())
-A = list(map(int,input().split()))
-B = list(map(int,input().split()))
-if A in B:
+#A = list(map(int,input().split()))
+#B = list(map(int,input().split()))
+A = input()
+B = input()
+if B in A:
     print("Yes")
 else: print("No")
