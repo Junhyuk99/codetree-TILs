0ca3ee56bfa67afae94301ee9258d66b188ffc40
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-10-25 10:38:19 +0900

    Title: 문자열 놀이, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241025/문자열 놀이/play-with-string.py
@@ -0,0 +1,19 @@
+s,q = input().split()
+q = int(q)
+new = list(s)
+for _ in range(q):
+    arr = list(input().split())
+    if arr[0] == '1':
+        c = new[int(arr[1])-1]
+        new[int(arr[1])-1] = new[int(arr[2])-1]
+        new[int(arr[2])-1] = c
+        for ele in new:
+            print(ele, end='')
+        print()
+    elif arr[0] == '2':
+        for i in range(len(new)):
+            if new[i] == arr[1]:
+                new[i] = arr[2]
+        for ele in new:
+            print(ele, end='')
+            print()
