4c01b60d9674bc9723589896197646075c643ea3
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-10-28 08:46:30 +0900

    Title: 정수만 추출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241028/정수만 추출하기/extract-only-integers.py
+++ 241028/정수만 추출하기/extract-only-integers.py
@@ -2,11 +2,11 @@ a,b = input().split()
 n1 = 0
 n2 = 0
 for i in range(len(a)):
-    if a[i] <= '0' or a[i] >='9':
+    if a[i] < '0' or a[i] >'9':
         n1 = i
         break
 for i in range(len(b)):
-    if b[i] <= '0' or b[i] >='9':
+    if b[i] < '0' or b[i] >'9':
         n2 = i
         break
 a = a[:n1]
