b4f1a143ff91aa29d38a0994f167a2555da03677
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-11-21 09:18:59 +0900

    Title: 그 요일은, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241121/그 요일은/the-day-of-the-day.py
+++ 241121/그 요일은/the-day-of-the-day.py
@@ -4,7 +4,7 @@ A = input()
 
 def num_of_days(m, d):
     # 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
-    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     total_days = 0
     
     # 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
@@ -19,11 +19,8 @@ def num_of_days(m, d):
 
 # 두 날짜간의 차이가 몇 일인지를 구합니다.
 diff = num_of_days(m2, d2) - num_of_days(m1, d1)
-# 음수인 경우에는, 양수로 넘겨 계산해주면 올바르게 계산이 됩니다. 
-while diff < 0:
-    diff += 7
 
 # 알맞은 요일의 이름을 출력합니다.
 day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 diff_day = day_of_week.index(A)
-print((diff-diff_day+2)//7 )
+print((diff-diff_day + 7)//7 )
