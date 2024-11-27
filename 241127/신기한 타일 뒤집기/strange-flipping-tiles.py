bbbe2505938b5d8ec11a6025797519b73c660361
Junhyuk99 <48585810+Junhyuk99@users.noreply.github.com>
2024-11-27 09:19:46 +0900

    Title: 신기한 타일 뒤집기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241127/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 241127/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,25 +1,25 @@
-n = int(input())
-white = 0
-black = 0
-now = 0
-mini = 0
-maxi = 0
+n = int(input())  # 명령의 개수
+now = 0  # 현재 위치
+mini = 0  # 최소 위치
+maxi = 0  # 최대 위치
+
+# 명령 수행
 for i in range(n):
-    a,b = input().split()
+    a, b = input().split()
     a = int(a)
     if b == 'L':
         now -= a
-        if now <= mini:
-            mini = now
+        mini = min(mini, now)
     elif b == 'R':
         now += a
-        if now >= maxi:
-            maxi = now
-    if i == n-1:
-        if b == 'L':
-            white = maxi-now
-            black = now-mini
-        elif b == 'R':
-            white = maxi-now
-            black = now-mini
-print(white,black)
+        maxi = max(maxi, now)
+
+# 마지막 명령에 따른 타일 개수 계산
+if b == 'R':  # 마지막 명령이 R일 때
+    black = now - mini + 1
+    white = maxi - now
+elif b == 'L':  # 마지막 명령이 L일 때
+    black = now - mini
+    white = maxi - now + 1
+
+print(white, black)  # 검은색과 흰색 타일 출력
